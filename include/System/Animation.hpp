69caee769514f3ecb284e55fbc3d87222dbb85be
劉顥權 Haoquan Liu <haoquan291@gmail.com>
2023-12-25 17:34:24 +0800

    把視窗大小調整為 800 x 800 並把視窗邊界檢查重寫和加上角色轉向效果

---
+++ include/System/Animation.hpp
@@ -0,0 +1,8 @@
+class Animation {
+public:
+    Animation();
+    ~Animation();
+
+private:
+    
+};
--- src/Player.cpp
+++ src/Player.cpp
@@ -1,18 +1,30 @@
 #include <iostream>
+#include <algorithm>
 
 #include "Player.hpp"
 
+#define WINDOW_WIDTH 800
+#define WINDOW_HEIGHT 800
+
 void Player::initVariables() {
     this->movementSpeed = 10.f;
-    this->scale = 3.f;
+    this->scale = 10.f;
 }
 
 void Player::initSprite() {
-    if (!this->texture.loadFromFile("data/image/player/1.png")) {
+    if (!this->texture.loadFromFile("data/image/player/boat.png")) {
         std::cout << "Error: Could not load player texture" << std::endl;
     }
     this->sprite.setTexture(this->texture);
-    this->sprite.setScale(this->scale, this->scale);
+    // this->sprite.setScale(this->scale, this->scale);
+    this->sprite.setOrigin(
+        this->sprite.getGlobalBounds().width / 2,
+        this->sprite.getGlobalBounds().height / 2
+    );
+    this->sprite.setPosition(
+        WINDOW_WIDTH / 2,
+        WINDOW_HEIGHT / 2
+    );
 }
 
 Player::Player(float x, float y) {
@@ -28,39 +40,42 @@ Player::~Player() {
 
 // 鍵盤輸入
 void Player::updateInput() {
-    if (sf::Keyboard::isKeyPressed(sf::Keyboard::A)) {
+    if (sf::Keyboard::isKeyPressed(sf::Keyboard::A) && this->sprite.getPosition().x > (0.f + std::max(this->sprite.getGlobalBounds().width, this->sprite.getGlobalBounds().height) / 2)) {
         this->sprite.move(-this->movementSpeed, 0.f);
+        this->sprite.setRotation(90);
     }
-    if (sf::Keyboard::isKeyPressed(sf::Keyboard::D)) {
+    if (sf::Keyboard::isKeyPressed(sf::Keyboard::D) && this->sprite.getPosition().x < (WINDOW_WIDTH - std::max(this->sprite.getGlobalBounds().width, this->sprite.getGlobalBounds().height) / 2)) {
         this->sprite.move(this->movementSpeed, 0.f);
+        this->sprite.setRotation(270);
     }
-    if (sf::Keyboard::isKeyPressed(sf::Keyboard::W)) {
+    if (sf::Keyboard::isKeyPressed(sf::Keyboard::W) && this->sprite.getPosition().y > (0.f + std::max(this->sprite.getGlobalBounds().width, this->sprite.getGlobalBounds().height) / 2)) {
         this->sprite.move(0.f, -this->movementSpeed);
+        this->sprite.setRotation(180);
     }
-    if (sf::Keyboard::isKeyPressed(sf::Keyboard::S)) {
+    if (sf::Keyboard::isKeyPressed(sf::Keyboard::S) && this->sprite.getPosition().y < (WINDOW_HEIGHT - std::max(this->sprite.getGlobalBounds().width, this->sprite.getGlobalBounds().height) / 2)) {
         this->sprite.move(0.f, this->movementSpeed);
+        this->sprite.setRotation(0);
     }
 }
 
-// 碰撞邊界
-void Player::updateWindowBoundsCollision(const sf::RenderTarget* target) {
-    if (this->sprite.getGlobalBounds().left <= 0.f) {
-        this->sprite.setPosition(0.f, this->sprite.getGlobalBounds().top);
-    }
-    if (this->sprite.getGlobalBounds().left + this->sprite.getGlobalBounds().width >= target->getSize().x) {
-        this->sprite.setPosition(target->getSize().x - this->sprite.getGlobalBounds().width, this->sprite.getGlobalBounds().top);
-    }
-    if (this->sprite.getGlobalBounds().top <= 0.f) {
-        this->sprite.setPosition(this->sprite.getGlobalBounds().left, 0.f);
-    }
-    if (this->sprite.getGlobalBounds().top + this->sprite.getGlobalBounds().height >= target->getSize().y) {
-        this->sprite.setPosition(this->sprite.getGlobalBounds().left, target->getSize().y - this->sprite.getGlobalBounds().height);
-    }
+// 面對滑鼠
+void Player::updateMouseInput(const sf::RenderWindow &window) {
+    sf::Vector2f curPos = this->sprite.getPosition();
+    sf::Vector2i position = sf::Mouse::getPosition(window);
+
+    const float PI = 3.14159265;
+
+    float dx = curPos.x - position.x;
+    float dy = curPos.y - position.y;
+
+    float rotation = (atan2(dy, dx)) * 180 / PI;
+
+    this->sprite.setRotation(rotation + 90);
 }
 
-void Player::update(const sf::RenderTarget* target) {
+void Player::update(const sf::RenderWindow &window) {
     this->updateInput();
-    this->updateWindowBoundsCollision(target);
+    // this->updateMouseInput(window);
 }
 
 void Player::render(sf::RenderTarget* target) {
